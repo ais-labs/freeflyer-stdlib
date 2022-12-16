2f7272a97b207a4a98aa091059e23ad5bd24bea4
Justin Covey <justin.covey@ai-solutions.com>
2022-12-16 14:51:22 -0500

    Changing config to reference environment variable instead of assuming windows path to freeflyer home

--- python_freeflyer_interface/python_freeflyer_interface/config.py
+++ python_freeflyer_interface/python_freeflyer_interface/config.py
@@ -5,4 +5,4 @@ Configuration for python_freeflyer_interface
 # native imports
 import os
 
-FF_EXE_PATH = 'C:/Program Files/a.i. solutions, Inc/FreeFlyer 7.8.0.56460 (64-Bit)/'
+FF_EXE_PATH = os.environ['FF_HOME']
